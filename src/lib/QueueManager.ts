1991d45b9399d08d9d8cb3859aa0ed0ebb73b831
ShaunLWM <shaunlimweimin@gmail.com>
2021-08-07 17:56:58 +0800

    Emit full Pair on finish

--- src/lib/QueueManager.ts
+++ src/lib/QueueManager.ts
@@ -3,8 +3,8 @@ import { ethers } from "ethers";
 import ERC21Abi from "../abi/ERC21.json";
 import Pair, { IPair } from "../models/Pair.model";
 import Token, { IToken } from "../models/Token.model";
+import { io } from "../server";
 import { Provider } from "./Helpers";
-
 
 const PairQueue = new Queue("PairQueue");
 
@@ -14,104 +14,117 @@ const addPair = async (opts: PairOpts): Promise<void> => {
 };
 
 const PairWorker = new Worker<PairOpts>("PairQueue", async (job) => {
-	console.log(`[PairWorker] Processing queue ${JSON.stringify(job.data)}`);
-	const { token0, token1, pairAddress, date } = job.data;
-	const tokenZeroCOntract = new ethers.Contract(token0, ERC21Abi, Provider);
-	const tokenOneContract = new ethers.Contract(token1, ERC21Abi, Provider);
+	try {
+		console.log(`[PairWorker] Processing queue ${JSON.stringify(job.data)}`);
+		const { token0, token1, pairAddress, date } = job.data;
+		const tokenZeroCOntract = new ethers.Contract(token0, ERC21Abi, Provider);
+		const tokenOneContract = new ethers.Contract(token1, ERC21Abi, Provider);
 
-	const tokens = await Token.find({
-		address: { $in: [token0.toLowerCase(), token1.toLowerCase()] },
-	}).exec();
+		const tokens = await Token.find({
+			address: { $in: [token0.toLowerCase(), token1.toLowerCase()] },
+		})
+			.lean()
+			.exec();
 
-	let tokenHash = 0;
-	const searchTokens = [];
-	const pairDocument: Partial<IPair> = {
-		date,
-	};
+		let tokenHash = 0;
+		const searchTokens = [];
+		const pairDocument: Partial<IPair> = {
+			date,
+		};
 
-	const dbTokenZero = tokens.find((token) => token.address === token0.toLowerCase());
-	if (!dbTokenZero) {
-		tokenHash += 1;
-		searchTokens.push(...[tokenZeroCOntract.name(), tokenZeroCOntract.symbol()]);
-	} else {
-		pairDocument.token0 = dbTokenZero._id;
-	}
-
-	const dbTokenOne = tokens.find((token) => token.address === token1.toLowerCase());
-	if (!dbTokenOne) {
-		tokenHash += 2;
-		searchTokens.push(...[tokenOneContract.name(), tokenOneContract.symbol()]);
-	} else {
-		pairDocument.token1 = dbTokenOne._id;
-	}
-
-	if (tokenHash === 0) return console.log("Nothing to search");
-
-	const results = (await Promise.allSettled(searchTokens)).filter((p) => p.status === "fulfilled");
-	if (results.length !== searchTokens.length) return console.log("Something broke");
-
-	if (
-		results[0].status === "rejected" ||
-		results[1].status === "rejected" ||
-		(results[2] && results[2].status === "rejected") ||
-		(results[3] && results[3].status === "rejected")
-	)
-		return;
-
-	const tokenDocuments: IToken[] = [];
-	switch (tokenHash) {
-		case 1: // token0 only
-		case 2:
-			tokenDocuments.push({
-				address: tokenHash === 1 ? token0.toLowerCase() : token1.toLowerCase(),
-				name: results[0].value.trim(),
-				symbol: results[1].value.trim(),
-			});
-			break;
-		case 3:
-			tokenDocuments.push({
-				address: token0.toLowerCase(),
-				name: results[0].value.trim(),
-				symbol: results[1].value.trim(),
-			});
-			tokenDocuments.push({
-				address: token1.toLowerCase(),
-				name: results[2].value.trim(),
-				symbol: results[3].value.trim(),
-			});
-			break;
-		default:
-			throw new Error("Shouldnt even be here");
-	}
-
-	const tokensInsertResult = await Token.insertMany(tokenDocuments);
-	console.log(tokensInsertResult);
-	switch (tokenHash) {
-		case 1:
-			pairDocument.token0 = tokensInsertResult[0]._id;
-			break;
-		case 2:
-			pairDocument.token1 = tokensInsertResult[0]._id;
-			break;
-		case 3:
-			pairDocument.token0 = tokensInsertResult[0]._id;
-			pairDocument.token1 = tokensInsertResult[1]._id;
-			break;
-	}
-
-	const pairUpsertResult = await Pair.findOneAndUpdate(
-		{
-			address: pairAddress.toLowerCase(),
-		},
-		pairDocument,
-		{
-			upsert: true,
-			new: true,
-			setDefaultsOnInsert: true,
+		const dbTokenZero = tokens.find((token) => token.address === token0.toLowerCase());
+		if (!dbTokenZero) {
+			tokenHash += 1;
+			searchTokens.push(...[tokenZeroCOntract.name(), tokenZeroCOntract.symbol()]);
+		} else {
+			pairDocument.token0 = dbTokenZero._id;
 		}
-	).exec();
-	console.log(pairUpsertResult);
-	console.log(`---------------- DONE ----------------`);
+
+		const dbTokenOne = tokens.find((token) => token.address === token1.toLowerCase());
+		if (!dbTokenOne) {
+			tokenHash += 2;
+			searchTokens.push(...[tokenOneContract.name(), tokenOneContract.symbol()]);
+		} else {
+			pairDocument.token1 = dbTokenOne._id;
+		}
+
+		if (tokenHash === 0) return console.log("Nothing to search");
+
+		const results = (await Promise.allSettled(searchTokens)).filter((p) => p.status === "fulfilled");
+		if (results.length !== searchTokens.length) return console.log("Something broke");
+
+		if (
+			results[0].status === "rejected" ||
+			results[1].status === "rejected" ||
+			(results[2] && results[2].status === "rejected") ||
+			(results[3] && results[3].status === "rejected")
+		)
+			return;
+
+		const tokenDocuments: IToken[] = [];
+		switch (tokenHash) {
+			case 1: // token0 only
+			case 2:
+				tokenDocuments.push({
+					address: tokenHash === 1 ? token0.toLowerCase() : token1.toLowerCase(),
+					name: results[0].value.trim(),
+					symbol: results[1].value.trim(),
+				});
+				break;
+			case 3:
+				tokenDocuments.push({
+					address: token0.toLowerCase(),
+					name: results[0].value.trim(),
+					symbol: results[1].value.trim(),
+				});
+				tokenDocuments.push({
+					address: token1.toLowerCase(),
+					name: results[2].value.trim(),
+					symbol: results[3].value.trim(),
+				});
+				break;
+			default:
+				throw new Error("Shouldnt even be here");
+		}
+
+		const tokensInsertResult = await Token.insertMany(tokenDocuments);
+		console.log(tokensInsertResult);
+		switch (tokenHash) {
+			case 1:
+				pairDocument.token0 = tokensInsertResult[0]._id;
+				break;
+			case 2:
+				pairDocument.token1 = tokensInsertResult[0]._id;
+				break;
+			case 3:
+				pairDocument.token0 = tokensInsertResult[0]._id;
+				pairDocument.token1 = tokensInsertResult[1]._id;
+				break;
+		}
+
+		const pairUpsertResult = await Pair.findOneAndUpdate(
+			{
+				address: pairAddress.toLowerCase(),
+			},
+			pairDocument,
+			{
+				upsert: true,
+				new: true,
+				setDefaultsOnInsert: true,
+			}
+		).exec();
+		console.log(pairUpsertResult);
+		const result = await Pair.findOne({ _id: pairUpsertResult._id })
+			.populate("token0", "-_id -__v")
+			.populate("token1", "-_id -__v")
+			.select("-__v")
+			.lean()
+			.exec();
+		console.log(`---------------- DONE ----------------`);
+		io.emit("pair:new", result);
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 PairWorker.on("error", (err) => {
