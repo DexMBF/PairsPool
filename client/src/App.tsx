17822ffc6584e1ffe066aaaa242acf3a17061b1e
ShaunLWM <shaunlimweimin@gmail.com>
2021-08-07 22:28:13 +0800

    Store new pairs in state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,20 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { WebSocketContext } from "./providers/WebSockerProvider";
+import clone from "lodash.clonedeep";
 
 function App() {
 	const { socket, connected } = useContext(WebSocketContext);
+	const [pairs, setPairs] = useState([]);
+
 	useEffect(() => {
 		if (!connected) return;
 		socket?.on("pair:new", (msg) => {
-			console.log(msg);
+			const cloned = clone(pairs);
+			cloned.push(msg);
+			setPairs(cloned.slice(0, 25));
 		});
-	}, [connected, socket]);
+	}, [connected, pairs, socket]);
+
 	return <div className="App"></div>;
 }
 
