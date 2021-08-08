8ff6e1cb93ffd168e4a4e2a3ff6343bebcd4a09f
ShaunLWM <shaunlimweimin@gmail.com>
2021-08-08 10:02:53 +0800

    Add simple table for new emitted pairs

---
+++ client/src/@types/index.d.ts
@@ -0,0 +1,17 @@
+interface IPair {
+	address: string;
+	token0: string;
+	token1: string;
+	date: number;
+}
+
+interface IToken {
+	address: string;
+	name: string;
+	symbol: string;
+}
+
+interface PairEmitData extends Omit<IPair, "token0" | "token1"> {
+	token0: IToken;
+	token1: IToken;
+}
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,49 @@
+import { Table, TableCaption, Tbody, Td, Th, Thead, Tr } from "@chakra-ui/react";
+import clone from "lodash.clonedeep";
 import React, { useContext, useEffect, useState } from "react";
 import { WebSocketContext } from "./providers/WebSockerProvider";
-import clone from "lodash.clonedeep";
 
 function App() {
 	const { socket, connected } = useContext(WebSocketContext);
-	const [pairs, setPairs] = useState([]);
+	const [pairs, setPairs] = useState<PairEmitData[]>([]);
 
 	useEffect(() => {
 		if (!connected) return;
-		socket?.on("pair:new", (msg) => {
+		socket?.on("pair:new", (msg: PairEmitData) => {
 			const cloned = clone(pairs);
-			cloned.push(msg);
+			cloned.unshift(msg);
 			setPairs(cloned.slice(0, 25));
 		});
 	}, [connected, pairs, socket]);
 
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<Table variant="simple">
+				<TableCaption>Imperial to metric conversion factors</TableCaption>
+				<Thead>
+					<Tr>
+						<Th>To convert</Th>
+						<Th>into</Th>
+						<Th isNumeric>multiply by</Th>
+					</Tr>
+				</Thead>
+				<Tbody>
+					{pairs.length > 0 &&
+						pairs.map((pair) => {
+							return (
+								<Tr>
+									<Td>{pair.address}</Td>
+									<Td>
+										{pair.token0.name}/{pair.token1.name}
+									</Td>
+									<Td>{pair.date}</Td>
+								</Tr>
+							);
+						})}
+				</Tbody>
+			</Table>
+		</div>
+	);
 }
 
 export default App;
