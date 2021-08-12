23bcff7b6f86f7b183e8ce01bf9ee0e40cd6378b
ShaunLWM <shaunlimweimin@gmail.com>
2021-08-12 21:06:47 +0800

    Add prod env with https certs

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,23 @@
 import http from "http";
+import https from "https";
 import { Server } from "socket.io";
-const server = http.createServer();
+import fs from "fs";
+
+let server;
+
+if (process.env.ENVIRONMENT && process.env.ENVIRONMENT === "prod") {
+	console.log("[server] prod environment");
+	const opts = {
+		key: fs.readFileSync(process.env.SSL_KEY as string),
+		cert: fs.readFileSync(process.env.SSL_CERT as string),
+		ca: fs.readFileSync(process.env.SSL_CHAIN as string),
+	};
+	server = https.createServer(opts);
+} else {
+	console.log("[server] dev environment");
+	server = http.createServer();
+}
+
 const io = new Server(server, {
 	cors: {
 		origin: process.env.CORS_WHITELIST?.split(",") ?? "*",
